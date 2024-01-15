2509896f39f72078cba783ddbba34582870038c7
joe senenfelder <est2020.joe@gmail.com>
2024-01-15 01:08:57 -0600

    Add files via upload

---
+++ sfv.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	directoryPath := "."
+
+	_, err :=os.Stat(directoryPath)
+	if os.IsNotExist(err) {
+		fmt.Printf("Directory '%s' not found.\n", directoryPath)
+		return
+	}
+	
+
+	fileServer := http.FileServer(http.Dir(directoryPath))
+
+	http.Handle("/", fileServer)
+
+	port :=8080
+	fmt.Printf("Server started at http://localhost:%d\n", port)
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+	}
+}
